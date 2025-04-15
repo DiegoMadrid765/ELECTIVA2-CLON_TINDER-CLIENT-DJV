8a65f5b2362f977a1355b24b5ae30614eb2a9b6e
Diego Madrid <analista8@mic.com.co>
2025-04-15 08:10:29 -0500

    Se añaden sockets

--- src/app/dashboard/pages/login/login.component.ts
+++ src/app/dashboard/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SocketService } from '../../../services/socket.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  /**
+   *
+   */
+  message = '';
+  messages: string[] = [];
+  private room = 'sala1';
+  private subscription!: Subscription;
+  constructor(private socketService: SocketService) {
+  }
 
+  ngOnInit() {
+    this.socketService.joinRoom("sala1");
+    this.subscription = this.socketService.onMessage().subscribe((msg) => {
+      console.log(msg);
+      this.messages.push(msg);
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+    this.socketService.disconnect();
+  }
+
+  send() {
+    this.socketService.sendMessage("sala1", { id: 1, name: "Diego", message: "hola como estas" });
+    this.message = '';
+  }
 }
